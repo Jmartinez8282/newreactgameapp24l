7d42d7a5e293a441834ded4a1eb3b34b6ae274d2
Jose Luis Martinez Lopez <josemartinez8282@gmail.com>
2024-07-02 18:50:12 -0700

    created build folder dist

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,7 +1,7 @@
 ///All our imports we need
 
 import { SimpleGrid, Text } from "@chakra-ui/react";
-import useGames, { Platform } from "../hooks/useGames";
+import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
 import GameCardContainer from "./GameCardContainer";
